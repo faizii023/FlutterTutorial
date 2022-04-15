3ee934960a9112e7c91d3e75f2bec94e46a62d29
SHAIKH FAIZAN <faiziii642@gmail.com>
2022-04-15 16:47:25 +0530

    Check

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,70 @@
 import 'package:flutter/material.dart';
+// import 'package:flutter/cupertino.dart';
 
 void main() => runApp(MaterialApp(
       home: Home(),
     ));
 
 class Home extends StatelessWidget {
+  void answerQuestion() {
+    print('Answer Chosen!');
+  }
+
   @override
   Widget build(BuildContext context) {
+    var question = [
+      "What\'s your favorite Color?",
+      "What\'s your favorite animal?",
+    ];
     return Scaffold(
       appBar: AppBar(
-        title: Text("Flutter App"),
-        backgroundColor: Colors.red[600],
-      ),
-      body: Center(
-        child: IconButton(
-          onPressed: (){},
-          icon: Icon(Icons.account_tree_rounded),
-
+        title: Text(
+          "Flutter App",
+          style: TextStyle(
+            fontFamily: 'Monsterrat',
+            fontStyle: FontStyle.italic,
+          ),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        child:Icon(Icons.add),
-        onPressed: () {},
         backgroundColor: Colors.red[600],
+      ),
+      body: Column(
+        children: [
+          Text(
+            'The question',
+            style: TextStyle(
+              fontFamily: 'Montserrat',
+              fontSize: 20.0,
+              letterSpacing: 1,
+            ),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              answerQuestion();
+            },
+            child: Text("Answer 1"),
+            style: ButtonStyle(
+              backgroundColor: MaterialStateProperty.all(Colors.red),
+            ),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              answerQuestion();
+            },
+            child: Text("Answer 2"),
+            style: ButtonStyle(
+              backgroundColor: MaterialStateProperty.all(Colors.red),
+            ),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              answerQuestion();
+            },
+            child: Text("Answer 3"),
+            style: ButtonStyle(
+              backgroundColor: MaterialStateProperty.all(Colors.red),
+            ),
+          ),
+        ],
       ),
     );
   }
